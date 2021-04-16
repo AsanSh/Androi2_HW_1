524fb17c59ee0d6ce6b75c85c9868649adb63127
шол <dxgfx@fdxd.jh>
2021-04-17 03:36:25 +0600

    Initial commit

---
+++ app/src/main/java/com/asan/android2/ui/profile/ProfileFragment.java
@@ -0,0 +1,56 @@
+package com.asan.android2.ui.profile;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.asan.android2.R;
+
+import static android.app.Activity.RESULT_OK;
+
+public class ProfileFragment extends Fragment {
+
+    ImageView imageView;
+    private static final int IMAGE_PICKER_CODE = 123;
+    Uri imageUri;
+
+    private ProfileViewModel profileViewModel;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        imageView = root.findViewById(R.id.imageView);
+        imageView.setOnClickListener(v ->{
+            imagePicker();
+        });
+        return root;
+}
+    private void imagePicker() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_PICKER_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+       if (requestCode == IMAGE_PICKER_CODE && resultCode == RESULT_OK && data != null) {
+           imageUri = data.getData();
+           imageView.setImageURI(imageUri);
+       }
+    }
+}
